7455b62e50246ec0a7d170148f64cad76ba0d137
minseo <kimminseo4216@gmail.com>
2026-05-02 01:25:48 +0900

    feat: 홈 페이지에 Popular on Netflix 섹션 추가

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,14 @@
 import BottomNav from "@/components/BottomNav";
+import MovieRow from "@/components/MovieRow";
 
 export default function HomePage() {
   return (
     <main>
-      {/* 페이지 내용 */}
+      {/* 페어 영역: Hero, Previews, Continue Watching */}
+
+      {/* 내 영역: Movie Rows */}
+      <MovieRow title="Popular on Netflix" />
+
       <BottomNav />
     </main>
   );
