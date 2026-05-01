f2bff6ad198f0d9d8a9f737a1332eb46afdf6db1
minseo <kimminseo4216@gmail.com>
2026-05-02 01:47:35 +0900

    feat: 홈 페이지 TMDB 데이터 SSR 연동

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,24 +1,57 @@
 import BottomNav from "@/components/BottomNav";
 import MovieRow from "@/components/MovieRow";
+import {
+  fetchAfricanMovies,
+  fetchNetflixOriginals,
+  fetchNowPlaying,
+  fetchPopular,
+  fetchTopRated,
+  fetchTrendingDay,
+  fetchTrendingWeek,
+  fetchTVThrillers,
+  fetchUSTVShows,
+} from "@/lib/tmdb";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const [
+    popular,
+    trending,
+    trendingDay,
+    topRated,
+    african,
+    netflixOriginals,
+    nowPlaying,
+    tvThrillers,
+    usTVShows,
+  ] = await Promise.all([
+    fetchPopular(),
+    fetchTrendingWeek(),
+    fetchTrendingDay(),
+    fetchTopRated(),
+    fetchAfricanMovies(),
+    fetchNetflixOriginals(),
+    fetchNowPlaying(),
+    fetchTVThrillers(),
+    fetchUSTVShows(),
+  ]);
+
   return (
     <main className="pb-20">
       {/* 페어 영역: Hero, Previews, Continue Watching */}
 
       {/* 내 영역: Movie Rows */}
       <div className="flex flex-col gap-6">
-        <MovieRow title="Popular on Netflix" />
-        <MovieRow title="Trending Now" />
-        <MovieRow title="Top 10 in Nigeria Today" />
-        <MovieRow title="My List" />
-        <MovieRow title="African Movies" />
-        <MovieRow title="Nollywood Movies & TV" />
-        <MovieRow title="Netflix Originals" />
-        <MovieRow title="Watch it Again" />
-        <MovieRow title="New Releases" />
-        <MovieRow title="TV Thrillers & Mysteries" />
-        <MovieRow title="US TV Shows" />
+        <MovieRow title="Popular on Netflix" medias={popular.results} />
+        <MovieRow title="Trending Now" medias={trending.results} />
+        <MovieRow title="Top 10 in Nigeria Today" medias={trendingDay.results} />
+        <MovieRow title="My List" medias={topRated.results} />
+        <MovieRow title="African Movies" medias={african.results} />
+        <MovieRow title="Nollywood Movies & TV" medias={african.results} />
+        <MovieRow title="Netflix Originals" medias={netflixOriginals.results} />
+        <MovieRow title="Watch it Again" medias={popular.results} />
+        <MovieRow title="New Releases" medias={nowPlaying.results} />
+        <MovieRow title="TV Thrillers & Mysteries" medias={tvThrillers.results} />
+        <MovieRow title="US TV Shows" medias={usTVShows.results} />
       </div>
 
       <BottomNav />
