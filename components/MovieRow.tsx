67f713a3300b393bdaa9c4cfd4a3525f3420ef1d
minseo <kimminseo4216@gmail.com>
2026-05-02 01:25:37 +0900

    style: MovieRow 제목 좌측 패딩 16px 조정

--- components/MovieRow.tsx
+++ components/MovieRow.tsx
@@ -8,7 +8,7 @@ type MovieRowProps = {
 export default function MovieRow({ title, count = 10 }: MovieRowProps) {
   return (
     <section className="flex flex-col gap-3">
-      <h2 className="text-heading2 text-white pl-3">{title}</h2>
+      <h2 className="text-heading2 text-white pl-4">{title}</h2>
       <div className="flex gap-[7px] overflow-x-auto pl-3">
         {Array.from({ length: count }).map((_, i) => (
           <MovieCard key={i} />
