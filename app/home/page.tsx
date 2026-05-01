0896a922f4fbae85874c83affae11717decf55f3
minseo <kimminseo4216@gmail.com>
2026-05-02 01:29:03 +0900

    feat: 홈 페이지에 Movie Row 카테고리 추가

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,7 +7,19 @@ export default function HomePage() {
       {/* 페어 영역: Hero, Previews, Continue Watching */}
 
       {/* 내 영역: Movie Rows */}
-      <MovieRow title="Popular on Netflix" />
+      <div className="flex flex-col gap-6">
+        <MovieRow title="Popular on Netflix" />
+        <MovieRow title="Trending Now" />
+        <MovieRow title="Top 10 in Nigeria Today" />
+        <MovieRow title="My List" />
+        <MovieRow title="African Movies" />
+        <MovieRow title="Nollywood Movies & TV" />
+        <MovieRow title="Netflix Originals" />
+        <MovieRow title="Watch it Again" />
+        <MovieRow title="New Releases" />
+        <MovieRow title="TV Thrillers & Mysteries" />
+        <MovieRow title="US TV Shows" />
+      </div>
 
       <BottomNav />
     </main>
