275546ca891515edb8733f63d8ae5212f183f757
minseo <kimminseo4216@gmail.com>
2026-05-02 01:45:21 +0900

    refactor: MovieRow가 medias 배열 받도록 변경

--- components/MovieRow.tsx
+++ components/MovieRow.tsx
@@ -1,17 +1,18 @@
 import MovieCard from "./MovieCard";
+import type { Media } from "@/types/media";
 
 type MovieRowProps = {
   title: string;
-  count?: number;
+  medias?: Media[];
 };
 
-export default function MovieRow({ title, count = 10 }: MovieRowProps) {
+export default function MovieRow({ title, medias }: MovieRowProps) {
   return (
     <section className="flex flex-col gap-3">
       <h2 className="text-heading2 text-white pl-4">{title}</h2>
       <div className="flex gap-[7px] overflow-x-auto pl-3 [&::-webkit-scrollbar]:hidden [scrollbar-width:none]">
-        {Array.from({ length: count }).map((_, i) => (
-          <MovieCard key={i} />
+        {medias?.map((media) => (
+          <MovieCard key={media.id} media={media} />
         ))}
       </div>
     </section>
