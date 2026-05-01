6b9482cd36d93b531391e5dedeb1c48a21e8b677
minseo <kimminseo4216@gmail.com>
2026-05-02 01:44:06 +0900

    feat: MovieCard에 포스터 이미지 표시 추가

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,26 @@
-export default function MovieCard() {
+import Image from "next/image";
+import type { Media } from "@/types/media";
+
+type MovieCardProps = {
+  media?: Media;
+};
+
+export default function MovieCard({ media }: MovieCardProps) {
+  if (!media?.poster_path) {
+    return <div className="w-[103px] h-[161px] rounded-[2px] bg-white shrink-0" />;
+  }
+
+  const title = media.title ?? media.name ?? "";
+
   return (
-    <div className="w-[103px] h-[161px] rounded-[2px] bg-white shrink-0" />
+    <div className="w-[103px] h-[161px] rounded-[2px] overflow-hidden shrink-0 relative">
+      <Image
+        src={`https://image.tmdb.org/t/p/w185${media.poster_path}`}
+        alt={title}
+        fill
+        sizes="103px"
+        className="object-cover"
+      />
+    </div>
   );
 }
