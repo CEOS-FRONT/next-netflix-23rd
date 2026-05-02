f71187a768bca186521b444128b8ed532f97e6b4
minseo <kimminseo4216@gmail.com>
2026-05-02 12:15:57 +0900

    style: HeroBanner #2 텍스트 디자인 적용

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,9 +1,27 @@
-export default function HeroBanner() {
+import Image from "next/image";
+import { getPosterUrl } from "@/lib/tmdb";
+import type { Media } from "@/types/media";
+
+type HeroBannerProps = {
+  media?: Media;
+};
+
+export default function HeroBanner({ media }: HeroBannerProps) {
+  const title = media?.title ?? media?.name ?? "";
+
   return (
     <div className="relative h-103.75 w-full bg-zinc-800">
-      {/* 사진 자리 - API 연결 후 채울 예정 */}
-      <div className="absolute inset-0" />
-      <span className="absolute bottom-0 left-1/2 -translate-x-1/2 text-heading2 text-white whitespace-nowrap">
+      {media?.poster_path && (
+        <Image
+          src={getPosterUrl(media.poster_path, "w780")}
+          alt={title}
+          fill
+          sizes="375px"
+          className="object-cover"
+          priority
+        />
+      )}
+      <span className="absolute bottom-[2px] left-[138px] w-[123px] h-[20px] text-white text-center text-[13.723px] font-bold leading-5 tracking-[-0.041px] whitespace-nowrap z-10">
         #2 in Korea Today
       </span>
     </div>
