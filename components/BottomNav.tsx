21f7dfbac014c391a204e45f4e94c8adfdb77c66
girimNam <rlfla@naver.com>
2026-05-02 17:23:25 +0900

    chore: 하단 네비바 홈 인디케이터 설정

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -5,9 +5,13 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NAV_ITEMS = [
-  { icon: "/icons/icon-home.svg", label: "Home", href: "/home" },
+  { icon: "/icons/icon-home-filled.svg", label: "Home", href: "/home" },
   { icon: "/icons/icon-search.svg", label: "Search", href: "/search" },
-  { icon: "/icons/icon-comingsoon.svg", label: "Coming Soon", href: "/comingSoon" },
+  {
+    icon: "/icons/icon-comingsoon.svg",
+    label: "Coming Soon",
+    href: "/comingSoon",
+  },
   { icon: "/icons/icon-downloads.svg", label: "Downloads", href: "/downloads" },
   { icon: "/icons/icon-more.svg", label: "More", href: "/more" },
 ];
@@ -16,20 +20,25 @@ export default function BottomNav() {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed bottom-0 flex w-93.75 h-14 px-6 bg-black justify-between items-center">
-      {NAV_ITEMS.map(({ icon, label, href }) => {
-        const isActive = pathname === href;
-        return (
-          <Link
-            key={href}
-            href={href}
-            className={`flex flex-col items-center gap-1 text-[8px] ${isActive ? "text-white" : "text-Grey-700"}`}
-          >
-            <Image src={icon} alt={label} width={24} height={24} />
-            <span>{label}</span>
-          </Link>
-        );
-      })}
+    <nav className="fixed bottom-0 flex flex-col w-93.75 bg-black">
+      <div className="flex h-14 px-6 justify-between items-center">
+        {NAV_ITEMS.map(({ icon, label, href }) => {
+          const isActive = pathname === href;
+          return (
+            <Link
+              key={href}
+              href={href}
+              className={`flex flex-col items-center gap-1 text-[8px] ${isActive ? "text-white" : "text-Grey-700"}`}
+            >
+              <Image src={icon} alt={label} width={18} height={18} />
+              <span>{label}</span>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="flex justify-center pb-2 mt-4">
+        <div className="w-30 h-1 bg-white rounded-full" />
+      </div>
     </nav>
   );
 }
