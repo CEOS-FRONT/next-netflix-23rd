8edd2f20415e79eeef2453a6e0d035a588d352f5
girimNam <rlfla@naver.com>
2026-05-02 10:52:54 +0900

    feat: 홈 페이지에 상단 컴포넌트 추가

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,19 @@
+import HomeHeader from "@/components/HomeHeader";
+import HeroBanner from "@/components/HeroBanner";
+import BannerButtons from "@/components/BannerButtons";
+import Previews from "@/components/Previews";
+import ContinueWatching from "@/components/ContinueWatching";
+import BottomNav from "@/components/BottomNav";
+
 export default function HomePage() {
-  return <main></main>;
+  return (
+    <main>
+      <HomeHeader />
+      <HeroBanner />
+      <BannerButtons />
+      <Previews />
+      <ContinueWatching />
+      <BottomNav />
+    </main>
+  );
 }
