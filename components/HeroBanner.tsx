f01fe97abad8d34c0f4a351fa058ed2744947343
minseo <kimminseo4216@gmail.com>
2026-05-02 12:30:39 +0900

    feat: HeroBanner에 TOP 10 아이콘 추가

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -21,6 +21,13 @@ export default function HeroBanner({ media }: HeroBannerProps) {
           priority
         />
       )}
+      <Image
+        src="/icons/icon-top10.svg"
+        alt="TOP 10"
+        width={15}
+        height={15}
+        className="absolute bottom-[4px] left-[117px] z-10"
+      />
       <span className="absolute bottom-[2px] left-[138px] w-[123px] h-[20px] text-white text-center text-[13.723px] font-bold leading-5 tracking-[-0.041px] whitespace-nowrap z-10">
         #2 in Korea Today
       </span>
