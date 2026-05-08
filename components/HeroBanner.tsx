a3e27abb808aa79bd911e88a040688cdd06f6a40
minseo <kimminseo4216@gmail.com>
2026-05-08 14:38:01 +0900

    refactor: SVG 아이콘 컴포넌트화

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { getPosterUrl } from "@/lib/tmdb";
 import type { Media } from "@/types/media";
+import Top10Icon from "@/components/icons/Top10Icon";
 
 type HeroBannerProps = {
   medias?: Media[];
@@ -37,12 +38,10 @@ export default function HeroBanner({ medias = [] }: HeroBannerProps) {
           priority
         />
       )}
-      <Image
-        src="/icons/icon-top10.svg"
-        alt="TOP 10"
-        width={15}
-        height={15}
-        className="absolute bottom-1 left-29.25 z-10"
+      <Top10Icon 
+        width={15} 
+        height={15} 
+        className="absolute bottom-1 left-29.25 z-10" 
       />
       <span className="absolute bottom-0.5 left-34.5 h-5 text-white text-center text-[13.723px] font-bold leading-5 tracking-[-0.041px] whitespace-nowrap z-10">
         #{currentIndex + 1} in Korea Today
