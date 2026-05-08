cf099dc9b8d4b35ad0c02415300cdd79bdef9bfb
minseo <kimminseo4216@gmail.com>
2026-05-08 13:55:30 +0900

    feat: 상세 페이지 라우트 생성

---
+++ app/detail/[id]/page.tsx
@@ -0,0 +1,26 @@
+import DetailHeroImage from "@/components/DetailHeroImage";
+import DetailInfo from "@/components/DetailInfo";
+import PlayButton from "@/components/PlayButton";
+import { fetchMediaDetail } from "@/lib/tmdb";
+
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ type?: string }>;
+};
+
+export default async function DetailPage({ params, searchParams }: Props) {
+  const { id } = await params;
+  const { type } = await searchParams;
+  const mediaType = type === "tv" ? "tv" : "movie";
+
+  const media = await fetchMediaDetail(Number(id), mediaType);
+  const title = media.title ?? media.name ?? "";
+
+  return (
+    <main className="bg-black min-h-screen">
+      <DetailHeroImage media={media} />
+      <PlayButton />
+      <DetailInfo title={title} overview={media.overview} />
+    </main>
+  );
+}
